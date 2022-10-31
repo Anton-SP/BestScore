d3e43a5841e1c2f7f3fda8a7691c71a5e00abfaa
Dmitriy Negrobov <dv.negrobov@gmail.com>
2022-10-31 19:59:08 +0300

    Добавляет фрагмент создания шаблона

---
+++ featureCreateTemplate/src/main/java/com/bestscore/featurecreatetemplate/CreateTemplateFragment.kt
@@ -0,0 +1,11 @@
+package com.bestscore.featurecreatetemplate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CreateTemplateFragment : Fragment() {
+
+}
