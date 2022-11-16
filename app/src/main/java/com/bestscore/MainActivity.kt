25d287e7510d60aaf54b423047e39a8508d28a4d
toha <toha1988cool@gmail.com>
2022-11-16 13:30:05 +0300

    feature/start_screen fix

--- app/src/main/java/com/bestscore/MainActivity.kt
+++ app/src/main/java/com/bestscore/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.bestscore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bestscore.featurecreatetemplate.CreateTemplateFragment
+import androidx.appcompat.app.AppCompatActivity
 import com.example.featurestartscreen.StartScreenFragment
 
 class MainActivity : AppCompatActivity() {
