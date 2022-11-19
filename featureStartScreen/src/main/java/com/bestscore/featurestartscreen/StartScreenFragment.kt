742543a1c9f44dd7058dcc7e6ca277eba26ceca6
AntonPetrov <pasmobiledev@gmail.com>
2022-11-19 21:44:31 +0300

    start_screen_di init

---
+++ featureStartScreen/src/main/java/com/bestscore/featurestartscreen/StartScreenFragment.kt
@@ -0,0 +1,26 @@
+package com.bestscore.featurestartscreen
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.bestscore.core.navigation.navigate
+import com.example.featurestartscreen.R
+import com.example.featurestartscreen.databinding.FragmentStartScreenBinding
+
+class StartScreenFragment : Fragment(R.layout.fragment_start_screen) {
+    private val binding: FragmentStartScreenBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            buttonCreateTemplate.setOnClickListener {
+                navigate(R.id.action_startScreenFragment_to_createTemplateFragment)
+            }
+            buttonMyTemplates.setOnClickListener {
+                navigate(R.id.action_startScreenFragment_to_templatesListFragment)
+            }
+        }
+    }
+}
