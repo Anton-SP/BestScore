c236449e4bf49077852a660e9507735526dab55f
Dmitriy Negrobov <dv.negrobov@gmail.com>
2022-11-26 20:40:56 +0300

    добавляет валидацию имен шаблона и параметров

---
+++ featureCreateTemplate/src/main/java/com/bestscore/featurecreatetemplate/Validator.kt
@@ -0,0 +1,33 @@
+package com.bestscore.featurecreatetemplate
+
+import com.bestscore.core.templates.Parameter
+import com.bestscore.core.templates.Template
+
+class Validator {
+    fun validate(template: Template, parameters: List<Parameter>): ValidationResult {
+        if (template.name.isBlank()) {
+            return ValidationResult(
+                success = false,
+                message = "Заполните имя шаблона"
+            )
+        }
+
+        parameters.forEach { parameter ->
+            if (parameter.parameterName.isBlank()) {
+                return ValidationResult(
+                    success = false,
+                    message = "Заполните имена параметров"
+                )
+            }
+        }
+
+        return ValidationResult(
+            success = true
+        )
+    }
+}
+
+data class ValidationResult(
+    val success: Boolean,
+    val message: String = ""
+)
