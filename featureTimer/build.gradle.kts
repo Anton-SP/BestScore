3d061c7920fe65884f301aa60b43074d87e49ab2
toha <toha1988cool@gmail.com>
2022-12-05 12:00:13 +0300

    feature/timer init

---
+++ featureTimer/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.bestscore.featuretimer"
+
+}
+
+dependencies {
+    //Modules
+    implementation(Modules.core)
+    implementation(Modules.utils)
+
+    //ViewBinding
+    implementation(ViewBinding.viewBinding)
+
+    //Fragment
+    implementation(Fragment.fragmentKtx)
+
+    //Coroutines
+    implementation(Kotlin.coroutinesCore)
+    implementation(Kotlin.coroutinesAndroid)
+
+    //Kotlin
+    implementation(Kotlin.core)
+
+    //AndroidX
+    implementation(Design.appcompat)
+
+    //Design
+    implementation(Design.material)
+
+    //Tests
+    testImplementation(TestImpl.junit)
+    androidTestImplementation(TestImpl.testExt)
+    androidTestImplementation(TestImpl.espresso)
+}
---
+++ featureTimer/src/main/java/com/bestscore/featuretimer/TimerDialogFragment.kt
@@ -0,0 +1,6 @@
+package com.bestscore.featuretimer
+
+import androidx.fragment.app.DialogFragment
+
+class TimerDialogFragment : DialogFragment(R.layout.fragment_timer){
+}
