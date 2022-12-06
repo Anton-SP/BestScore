ca072787163f71ed8a6f02956b0371e152efa022
Dmitriy Negrobov <dv.negrobov@gmail.com>
2022-12-06 20:50:39 +0300

    withContext

--- repository/src/main/java/com/bestscore/repository/RoomTemplateRepository.kt
+++ repository/src/main/java/com/bestscore/repository/RoomTemplateRepository.kt
@@ -1,32 +1,38 @@
 package com.bestscore.repository
 
+import com.bestscore.core.dispatchers.DispatchersProvider
 import com.bestscore.core.templates.Parameter
 import com.bestscore.core.templates.Template
 import com.bestscore.core.templates.TemplateRepository
 import com.bestscore.database.templates.TemplateDao
 import com.bestscore.utils.toEntity
 import com.bestscore.utils.toTemplate
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RoomTemplateRepository @Inject constructor(
-    private val dao: TemplateDao
+    private val dao: TemplateDao,
+    private val dispatchersProvider: DispatchersProvider
 ) : TemplateRepository {
-    override suspend fun create(template: Template, parameters: List<Parameter>): Long {
-        val templateId = dao.insertTemplate(template = template.toEntity())
-        if (templateId > 0) {
-            dao.insertParametersList(parameters = parameters.map { it.toEntity(templateId) })
-            return templateId
+
+    override suspend fun create(template: Template, parameters: List<Parameter>): Long =
+        withContext(dispatchersProvider.io()) {
+            val templateId = dao.insertTemplate(template = template.toEntity())
+            if (templateId > 0) {
+                dao.insertParametersList(parameters = parameters.map { it.toEntity(templateId) })
+                return@withContext templateId
+            }
+
+            return@withContext templateId
         }
 
-        return templateId
+    override suspend fun getTemplates(): List<Template> = withContext(dispatchersProvider.io()) {
+        return@withContext dao.getTemplateList().map { entity -> entity.toTemplate() }
     }
 
-    override suspend fun getTemplates(): List<Template> {
-        return dao.getTemplateList().map { entity -> entity.toTemplate() }
-    }
-
-    override suspend fun getLatestTemplates(): List<Template> {
-       return dao.getLatestTemplateList().map { entity -> entity.toTemplate() }
-    }
+    override suspend fun getLatestTemplates(): List<Template> =
+        withContext(dispatchersProvider.io()) {
+            return@withContext dao.getLatestTemplateList().map { entity -> entity.toTemplate() }
+        }
 
 }
