7b7bae49733fb780a718003640590444dda4ef68
Dmitriy Negrobov <dv.negrobov@gmail.com>
2022-12-10 10:12:27 +0300

    with context

--- repository/src/main/java/com/bestscore/repository/RoomTemplateRepository.kt
+++ repository/src/main/java/com/bestscore/repository/RoomTemplateRepository.kt
@@ -14,33 +14,37 @@ class RoomTemplateRepository @Inject constructor(
     private val dao: TemplateDao,
     private val dispatchersProvider: DispatchersProvider
 ) : TemplateRepository {
-    override suspend fun create(template: Template): Long {
+    override suspend fun create(template: Template): Long  = withContext(dispatchersProvider.io()){
         val templateId = dao.insertTemplate(template = template.toEntity())
         if (templateId > 0) {
             dao.insertParametersList(parameters = template.parameters.map { it.toEntity(templateId) })
-            return templateId
+            return@withContext templateId
         }
 
-        return -1
+        return@withContext TEMPLATE_WAS_NOT_CREATED
     }
 
-    override suspend fun getTemplates(): List<Template> {
-        return dao.getTemplateList().map { entity ->
+    override suspend fun getTemplates(): List<Template> = withContext(dispatchersProvider.io()) {
+        return@withContext dao.getTemplateList().map { entity ->
             entity.toTemplate(
                 dao.getTemplateParameters(entity.id).toParameters()
             )
         }
     }
 
-    override suspend fun getLatestTemplates(): List<Template> {
-        return dao.getLatestTemplateList().map { entity ->
+    override suspend fun getLatestTemplates(): List<Template> = withContext(dispatchersProvider.io()) {
+        return@withContext dao.getLatestTemplateList().map { entity ->
             entity.toTemplate(
                 dao.getTemplateParameters(entity.id).toParameters()
             )
         }
     }
 
-    override suspend fun delete(template: Template): Int {
-        return dao.delete(template.toEntity())
+    override suspend fun delete(template: Template): Int = withContext(dispatchersProvider.io()){
+        return@withContext dao.delete(template.toEntity())
+    }
+
+    companion object {
+        const val TEMPLATE_WAS_NOT_CREATED = -1L
     }
 }
